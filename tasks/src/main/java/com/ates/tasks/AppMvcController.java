da5f7ee9ddeba26b6028526cdf118a6a01a8367a
Pavel Tishkevich <pavel.tishkevich@onde.app>
2022-05-09 20:44:42 +0300

    Ability of authenticated user to view tasks assigned to him

--- tasks/src/main/java/com/ates/tasks/AppMvcController.java
+++ tasks/src/main/java/com/ates/tasks/AppMvcController.java
@@ -2,13 +2,20 @@ package com.ates.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.logging.StreamHandler;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping("/tasks")
@@ -19,11 +26,25 @@ public class AppMvcController {
     @GetMapping("/assign")
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_EMPLOYEE')")
     public String assignTasks(Model model) {
+
         List<Task> tasks = new ArrayList<>();
         taskRepository
                 .findAll()
                 .forEach(tasks::add);
         model.addAttribute("tasks", tasks);
         return "assign";
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
+    public String listTasks(Model model, Principal principal) {
+        UUID profileId = UUID.fromString(principal.getName());
+        List<Task> tasks = StreamSupport
+                .stream(taskRepository.findAll().spliterator(), false)
+                .filter(task -> profileId.equals(task.getAssigneeId()))
+                .collect(Collectors.toList());
+        model.addAttribute("tasks", tasks);
+
+        return "tasks";
     }
 }
