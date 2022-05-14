0e7d680b7ae4390d50125b42b58d89c64a082517
Pavel Tishkevich <pavel.tishkevich@onde.app>
2022-05-14 16:28:03 +0300

    Consuming profile stream in billing app

--- billing/src/main/java/com/ates/billing/BillingApplication.java
+++ billing/src/main/java/com/ates/billing/BillingApplication.java
@@ -1,13 +1,30 @@
 package com.ates.billing;
 
+import com.ates.billing.profile.Profile;
+import com.ates.billing.profile.ProfileRepository;
+import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication
 public class BillingApplication {
+
+	@Autowired
+	ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingApplication.class, args);
@@ -18,4 +35,51 @@ public class BillingApplication {
 		return new KeycloakSpringBootConfigResolver();
 	}
 
+	@KafkaListener(topics = "profile-stream", groupId = "billing_profile")
+	public void listen(@Payload ConsumerRecord<String, DynamicMessage> data, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key) throws InvalidProtocolBufferException {
+		System.out.println("############## Received message key= " + key + " , value= " + data);
+		DynamicMessage message = data.value();
+		String messageType = message.getDescriptorForType().getFullName();
+
+		if ("profile.Created".equals(messageType)) {
+			handleProfileCreated(message);
+		}
+		else if ("profile.RoleUpdated".equals(messageType)) {
+			handleProfileRoleUpdated(message);
+		}
+	}
+
+	private void handleProfileCreated(DynamicMessage message) throws InvalidProtocolBufferException {
+		com.ates.messages.Profile.Created profileCreatedMsg = com.ates.messages.Profile.Created
+				.newBuilder()
+				.build()
+				.getParserForType()
+				.parseFrom(message.toByteArray());
+		UUID profileId = UUID.fromString(profileCreatedMsg.getPublicId());
+		Profile profile = new Profile();
+		profile.setPublicId(profileId);
+		profile.setName(profileCreatedMsg.getName());
+
+		profileRepository.save(profile);
+	}
+
+	private void handleProfileRoleUpdated(DynamicMessage message) throws InvalidProtocolBufferException {
+		com.ates.messages.Profile.RoleUpdated roleUpdatedMsg = com.ates.messages.Profile.RoleUpdated
+				.newBuilder()
+				.build()
+				.getParserForType()
+				.parseFrom(message.toByteArray());
+		UUID profileId = UUID.fromString(roleUpdatedMsg.getPublicId());
+		Optional<Profile> profileForUpdate = StreamSupport
+				.stream(profileRepository.findAll().spliterator(), false)
+				.filter(profile -> profileId.equals(profile.getPublicId()))
+				.findFirst();
+
+		if (profileForUpdate.isPresent()) {
+			Profile profile = profileForUpdate.get();
+			profile.setRole(roleUpdatedMsg.getRole().name());
+
+			profileRepository.save(profile);
+		}
+	}
 }
---
+++ billing/src/main/java/com/ates/billing/profile/Profile.java
@@ -0,0 +1,51 @@
+package com.ates.billing.profile;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private UUID publicId;
+    private String name;
+    private String role;
+
+    public Profile() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public UUID getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(UUID publicId) {
+        this.publicId = publicId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
